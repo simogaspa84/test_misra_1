d0678a6c9973ef446a412edf703ce7b8f0a98bc0
Simone Gasparella <simone.gasparella@systel.it>
2022-12-29 11:21:06 +0100

    added debug

---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,50 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/analog.c \
+../Src/canmsg.c \
+../Src/eeprom.c \
+../Src/machine.c \
+../Src/main.c \
+../Src/oldflash2hal.c \
+../Src/stm32f1xx_hal_msp.c \
+../Src/stm32f1xx_it.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/system_stm32f1xx.c 
+
+OBJS += \
+./Src/analog.o \
+./Src/canmsg.o \
+./Src/eeprom.o \
+./Src/machine.o \
+./Src/main.o \
+./Src/oldflash2hal.o \
+./Src/stm32f1xx_hal_msp.o \
+./Src/stm32f1xx_it.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/system_stm32f1xx.o 
+
+C_DEPS += \
+./Src/analog.d \
+./Src/canmsg.d \
+./Src/eeprom.d \
+./Src/machine.d \
+./Src/main.d \
+./Src/oldflash2hal.d \
+./Src/stm32f1xx_hal_msp.d \
+./Src/stm32f1xx_it.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/system_stm32f1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F103xE -c -I../Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
